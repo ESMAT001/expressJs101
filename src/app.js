e5ad007b9c38208c78e477903995bb09da4bd9b6
ESMAT001 <esmat10.niazi@gmail.com>
2021-06-10 15:28:59 +0430

    code practice

--- src/app.js
+++ src/app.js
@@ -1,16 +1,18 @@
 const express = require('express');
 const fs = require('fs');
+const path = require('path');
 
 const app = express();
 
-const indexPage=fs.readFileSync(__dirname+'/index.html',{
-    encoding:"utf-8"
-})
 
-app.get('/', (req,res)=>{
+
+app.get('/', (req, res) => {
+    const indexPage = fs.readFileSync(path.join(__dirname, '../public/index.html'), {
+        encoding: "utf-8"
+    })
     res.send(indexPage)
 })
 
-app.listen(3000,()=>{
+app.listen(3000, () => {
     console.log("server started!")
 })
