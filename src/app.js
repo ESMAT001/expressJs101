0acee02103a710e66724049e5226870b0565981f
ESMAT001 <esmat10.niazi@gmail.com>
2021-06-10 15:33:17 +0430

    public directory added

--- src/app.js
+++ src/app.js
@@ -3,14 +3,15 @@ const fs = require('fs');
 const path = require('path');
 
 const app = express();
+const publicDirectory=path.join(__dirname,"../public")
 
-
+app.use(express.static(publicDirectory))
 
 app.get('/', (req, res) => {
-    const indexPage = fs.readFileSync(path.join(__dirname, '../public/index.html'), {
-        encoding: "utf-8"
-    })
-    res.send(indexPage)
+    // const indexPage = fs.readFileSync(path.join(__dirname, '../public/index.html'), {
+    //     encoding: "utf-8"
+    // })
+    res.send("<h1>index</h1>")
 })
 
 app.listen(3000, () => {
