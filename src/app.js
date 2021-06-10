0810218ef5d185bf8d5d562b63df4a6dfef6986b
ESMAT001 <esmat10.niazi@gmail.com>
2021-06-10 15:52:54 +0430

    code practice

--- src/app.js
+++ src/app.js
@@ -5,7 +5,12 @@ const path = require('path');
 const app = express();
 const publicDirectory=path.join(__dirname,"../public")
 
+app.set('view engine','hbs' )
 app.use(express.static(publicDirectory))
+
+app.get("",(req,res)=>{
+    res.render("index")
+})
 
 app.get('/help',(req,res)=>{
     res.send("help")
