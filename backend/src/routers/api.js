967854bb2c5ece3f35fe0d7145d7e05e482aab74
ESMAT001 <esmat10.niazi@gmail.com>
2021-06-27 21:09:29 +0430

    first api route functionality added successfully ✌️

--- backend/src/routers/api.js
+++ backend/src/routers/api.js
@@ -9,30 +9,54 @@ router.use(express.json())
 const retryLimit = 4
 const timeOutLimit = 20 * 1000
 
+const projectionFields = {
+    _id: false,
+    adult: false,
+    backdrop_path: "https://image.tmdb.org/t/p/w500/8ChCpCYxh9YXusmHwcE9YzP0TSG.jpg",
+    belongs_to_collection: false,
+    budget: false,
+    homepage: false,
+    imdb_id: false,
+    production_companies: false,
+    production_countries: false,
+    release_date: "2021-05-26",
+    revenue: false,
+    runtime: 134,
+    spoken_languages: false,
+    status: "Released",
+    tagline: "Hello Cruel World",
+    title: "Cruella",
+    video: false,
+    vote_average: 8.6,
+    vote_count: false,
+    videos: false,
+    download_links: false
+}
 
-async function getIds(page, movieCount, db) {
 
-    async function apiCallForIds(count, start = 0) {
-        let data = await got(metaData.getPopularURL(page)).json()
-        const results = data.results
+async function getIds(movieCount, db) {
+
+    async function apiCallForIds(movieCount) {
+        let page = 1
         const movieIds = []
-        let index;
-        for (let i = start; i < start + count; i++) {
-            const id = results[i].id
-            let movieDbData = await db.collection('movie').findOne({ id })
-            if (movieDbData) movieIds.push(id)
-            index = i
-        }
-        return {
-            index,
-            movieIds
-        }
+        do {
+            let data = await got(metaData.getTrendingURL(page)).json()
+            const results = data.results
+            for (let index = 0; index < results.length; index++) {
+                const id = results[index].id
+                let movieDbData = await db.collection('movie').findOne({ id })
+                if (movieDbData) movieIds.push(id)
+
+                if (movieIds.length >= movieCount) return movieIds;
+            }
+            page++
+        } while (movieIds.length < movieCount);
     }
 
 
 
-    const dbData = await db.collection("meta_data").findOne({ page })
-    let shouldUpdateData = true;
+    const dbData = await db.collection("meta_data").findOne({ name: "trending" })
+    let shouldUpdateData = false;
     if (dbData) {
         const lastUpdated = new Date(dbData.last_updated)
         lastUpdated.setDate(lastUpdated.getDate() + 1)
@@ -45,69 +69,51 @@ async function getIds(page, movieCount, db) {
         date.setDate(date.getDate() + 1)
         date = date.toUTCString()
         await db.collection("meta_data").insertOne({
-            page,
+            name: 'trending',
             last_updated: date
         })
         shouldUpdateData = true
     }
 
     if (shouldUpdateData) {
-
-
-        let movieIds = []
-
-        do {
-            let { index, movieIds: ids } = await apiCallForIds(movieCount, 0)
-        } while (movieCount <= ids.length);
-
-        // let data = await got(metaData.getPopularURL(page)).json()
-        // const results = data.results
-        // const movieIds = []
-
-        // results.forEach(async movie => {
-        //     const id = movie.id
-        //     let movieDbData = await db.collection('movie').findOne({ id })
-        //     if (movieDbData) movieIds.push(id)
-        // });
-
+        console.log('updating db for new trending data')
+        const movieIds = await apiCallForIds(movieCount)
+        await db.collection("meta_data").updateOne({
+            name: 'trending'
+        }, {
+            $set: { movieIds }
+        })
         return movieIds;
     } else {
-
+        const { movieIds } = await db.collection("meta_data").findOne({ name: "trending" })
+        const movieData = await db.collection("movie").find({
+            $and: [{
+                'id': { $in: movieIds }
+            }
+                ,
+            {
+                adult: false
+            }]
+        }, {
+            projection: projectionFields
+        }).toArray()
+        return movieData
     }
-
-
-
-
-
-    return true
 }
 
 
-async function fetchData(page = 1, db) {
-    const movieCount = page === 1 ? 8 : 12;
-    const movieIds = await getIds(page, movieCount, db)
-
-
-
-    let { body: data } = await got(metaData.getPopularURL(page))
-
+async function fetchData(db) {
+    const movieCount = 12;
+    const movieIds = await getIds(movieCount, db)
     return movieIds
 }
 
-router.get('/popular', async function (req, res) {
-    const { page = 1 } = req.query;
+router.get('/trending', async function (req, res) {
     const { db, close } = await connectToDb('media')
-    res.send(await fetchData(page, db))
+    res.send(await fetchData(db))
 })
 
-// router.get('/popularpage', async function (req, res) {
-//     const { page } = req.params
 
-//     if (!page) res.redirect('/popular')
 
-//     res.send(page)
-//     const { db, close } = await connectToDb('media')
-//     close()
-// })
 
 module.exports = router
--- backend/src/utils.js
+++ backend/src/utils.js
@@ -1,7 +1,7 @@
 const metaData = {
     apiKey: '3d97e93f74df6d3dd759d238a7b8564c',
-    getPopularURL(page = 1) {
-        return `https://api.themoviedb.org/3/movie/popular?api_key=${this.apiKey}&language=en-US&page=${page}`
+    getTrendingURL(page = 1) {
+        return `https://api.themoviedb.org/3/trending/movie/week?api_key=${this.apiKey}&page=${page}`
     }
 }
 
