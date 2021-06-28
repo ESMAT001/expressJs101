2ad249a8324cc3cf15ba4529607d31d291061c12
ESMAT001 <esmat10.niazi@gmail.com>
2021-06-28 17:29:16 +0430

    imges path fixed

---
+++ backend/src/temp.js
@@ -0,0 +1,25 @@
+const connectToDb = require('./db')
+
+async function main() {
+    const db = await connectToDb('media')
+
+    let data = await db.collection("movie").find({})
+    let count = 0
+    data.forEach(async (movie) => {
+        let id = movie.id
+
+        if (movie.poster_path === null || movie.backdrop_path === null) return;
+
+        let poster_path = movie.poster_path.split("/").pop()
+        let backdrop_path = movie.backdrop_path.split("/").pop()
+        await db.collection("movie").updateOne({ id }, { $set: { poster_path, backdrop_path } })
+        console.log(poster_path, backdrop_path, ++count)
+    })
+
+
+}
+
+
+
+
+main()
