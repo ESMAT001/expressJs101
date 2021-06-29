7d5c5056f34c6b75bcf62db2bec5f02ec28b4469
ESMAT001 <esmat10.niazi@gmail.com>
2021-06-29 12:05:35 +0430

    image endpoint added

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,11 +1,17 @@
 const express = require('express');
 const apiRouter = require('./routers/api')
+const imageRouter = require('./routers/image')
 
 const server = express()
 const port = 3000
 
 
 server.use('/api', apiRouter)
+server.use('/image',imageRouter)
+
+// server.get('/testImg', (req, res) => {
+//     res.send("<img src='image/z2UtGA1WggESspi6KOXeo66lvLx.jpg' alt='test'/>")
+// })
 
 server.get("*", (req, res) => {
     res.send("<h1>404 NOT FOUND!</h1>")
